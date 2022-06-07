fd91e7b87a9d78ca9db69c6a4db6bee3718eb96c
Tams-Tams <Tams.Mathe@gmail.com>
2022-06-07 13:58:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JSEvolve
+ An interactive evolution program that you can play around with in your web browser.
---
+++ sketch.js
@@ -0,0 +1,7 @@
+var canvasSize = window.innerHeight - 200;
+
+function setup() {
+  let canvas = createCanvas(canvasSize, canvasSize);
+  canvas.parent("canvas-div");
+  background(255, 255, 255);
+}
