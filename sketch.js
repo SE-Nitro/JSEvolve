823e3c47cc6c6c307c307bd44db0cac31e9525ed
Tams-Tams <Tams.Mathe@gmail.com>
2022-06-07 22:18:16 +0200

    -

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
-var canvasSize = window.innerHeight - 200;
+var canvasSize = window.innerWidth / 3;
+
+// function setup() {
+//   let canvas = createCanvas(canvasSize, canvasSize);
+//   canvas.parent("canvas-div");
+//   background(255, 255, 255);
+// }
 
 function setup() {
   let canvas = createCanvas(canvasSize, canvasSize);
   canvas.parent("canvas-div");
-  background(255, 255, 255);
+}
+
+function draw() {
+  background(255);
+  ellipse(width / 2, height / 2, 50, 50);
+}
+
+function windowResized() {
+  canvasSize = window.innerWidth / 3;
+  resizeCanvas(canvasSize, canvasSize);
 }
